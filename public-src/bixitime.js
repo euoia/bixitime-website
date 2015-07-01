8c8bf57aa51a24a598d45db939eef452c3013843
James Pickard <james.pickard@gmail.com>
2015-07-01 09:59:07 -0400

    responsive design

--- public-src/bixitime.js
+++ public-src/bixitime.js
@@ -82,10 +82,19 @@ BixiTime.prototype.showStations = function (position) {
 	$.getJSON(this.apiUrl, {
 		lat: position.coords.latitude,
 		long: position.coords.longitude,
+		limit: 5
 	}, function (stations) {
 		var stationSource = $('#station').html();
 		var stationTemplate = Handlebars.compile(stationSource);
+
+		// Render the template using station data.
 		var html = _.map(stations, function (station) {
+			if (station.distance > 2000) {
+				station.distance = '&gt; 2km';
+			} else {
+				station.distance = station.distance.toString() + 'm';
+			}
+
 			station.fromLat = position.coords.latitude;
 			station.fromLong = position.coords.longitude;
 			return stationTemplate(station);
