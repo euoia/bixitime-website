312f406b10123d5f2c16fe3b3b5a28a3a4cf358a
James Pickard <james.pickard@gmail.com>
2015-07-03 08:50:38 -0400

    Add debugging and error handling

--- public-src/bixitime.js
+++ public-src/bixitime.js
@@ -11,32 +11,49 @@ var BixiTime = module.exports = function (options) {
 	check.assert.string(options.apiUrl);
 	this.apiUrl = options.apiUrl;
 
+	this.debug = (location.search.match('debug=1') !== null);
+
 	this.loader = new Loader({
 		elementLocator: '.header'
 	});
+
+	if ('geolocation' in navigator === false) {
+		// TODO: Show unhappy map.
+		this.showSadMap('No gelocation available.');
+		return;
+	}
 
 	// Start the loading animation.
 	this.loader.startLoading();
 
 	var lastPositionItem = localStorage.getItem('position');
 	var lastPosition = null;
-	if (lastPositionItem !== null) {
+	if (lastPositionItem) {
 		try	{
 			lastPosition = JSON.parse(lastPositionItem);
 		} catch (e) {
+			if (this.debug) {
+				window.alert('Error parsing position from localStorage: ' +
+					JSON.stringify(lastPositionItem)
+				);
+			}
+
 			console.error('Error parsing position from localstorage', lastPositionItem, e);
 		}
 	}
 
-	if (lastPosition !== null) {
+	if (lastPosition !== null && this.validatePosition(lastPosition)) {
 		this.showStations(lastPosition);
 	}
 
-	navigator.geolocation.getCurrentPosition(
-		function (position) {
-			this.loader.stopLoading();
-			this.gotPosition(position);
-		}.bind(this)
+	navigator.geolocation.watchPosition(
+		this.gotPosition.bind(this),
+		this.showSadMap.bind(this),
+		{
+			timeout: 20000,
+			enableHighAccuracy: true,
+			maximumAge: 180000 // 3 minutes.
+		}
 	);
 
 	//this.gotPosition.bind({
@@ -47,38 +64,53 @@ var BixiTime = module.exports = function (options) {
 	//});
 };
 
-
 BixiTime.prototype.gotPosition = function(position) {
+	this.loader.stopLoading();
+	if (this.validatePosition(position) === false) {
+		this.showSadMap('Invalid position: ' + JSON.stringify(position));
+		return;
+	}
+
+	this.savePosition(position);
+	this.showStations(position);
+};
+
+BixiTime.prototype.showSadMap = function(error) {
+	this.loader.stopLoading();
+
+	if (this.debug) {
+		if (typeof error === 'object') {
+			error = JSON.stringify(error);
+		}
+		window.alert('Got an error: ' + error);
+	}
+
+	// TODO: Show a sad map.
+};
+
+
+BixiTime.prototype.validatePosition = function(position) {
 	if (position === undefined ||
 		position.coords === undefined ||
 		position.coords.latitude === undefined ||
 		position.coords.longitude === undefined
 	) {
-		console.error('Invalid position', position);
-		return;
+		return false;
 	}
 
+	return true;
+};
+
+BixiTime.prototype.savePosition = function(position) {
 	localStorage.setItem('position', JSON.stringify({
 		coords: {
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
 		}
 	}));
-
-	this.showStations(position);
-	$('.loading').hide();
 };
 
 BixiTime.prototype.showStations = function (position) {
-	if (position === undefined ||
-		position.coords === undefined ||
-		position.coords.latitude === undefined ||
-		position.coords.longitude === undefined
-	) {
-		console.error('Invalid position', position);
-		return;
-	}
-
 	$.getJSON(this.apiUrl, {
 		lat: position.coords.latitude,
 		long: position.coords.longitude,
